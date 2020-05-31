f04828f7abead0d4112e6dacb5ff966871220cdc
Ali Bulut <alibulut@yahoo.com>
2020-05-31 19:14:34 +0300

    outputting a list of places operations have been done

--- screens/PlacesListScreen.js
+++ screens/PlacesListScreen.js
@@ -1,14 +1,31 @@
 import React from "react";
-import { View, Text, StyleSheet, Platform } from "react-native";
+import { View, Text, StyleSheet, Platform, FlatList } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import { useSelector } from "react-redux";
 
 import CustomHeaderButton from "../components/CustomHeaderButton";
+import PlaceItem from "../components/PlaceItem";
 
 const PlacesListScreen = () => {
+  const places = useSelector((state) => state.places.places);
   return (
-    <View>
-      <Text>PlacesListScreen</Text>
-    </View>
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={(itemData) => (
+        <PlaceItem
+          image={null}
+          title={itemData.item.title}
+          address={null}
+          onSelect={() => {
+            props.navigation.navigate("PlaceDetail", {
+              placeTitle: itemData.item.title,
+              placeId: itemData.item.id,
+            });
+          }}
+        />
+      )}
+    />
   );
 };
 
@@ -21,7 +38,7 @@ PlacesListScreen.navigationOptions = (navData) => {
           title="Add Place"
           iconName={Platform.OS === "android" ? "md-add" : "ios-add"}
           onPress={() => {
-              navData.navigation.navigate('NewPlace')
+            navData.navigation.navigate("NewPlace");
           }}
         />
       </HeaderButtons>;

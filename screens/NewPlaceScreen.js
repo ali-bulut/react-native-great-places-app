8758e51a37b12c109c2cb597fa75dd650612ad75
Ali Bulut <alibulut@yahoo.com>
2020-05-31 18:41:29 +0300

    basic screen and navigation setup have been completed

---
+++ screens/NewPlaceScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const NewPlaceScreen = () => {
+    return (
+        <View>
+            <Text>NewPlaceScreen</Text>
+        </View>
+    );
+};
+
+NewPlaceScreen.navigationOptions = {
+    title:'Add Place'
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default NewPlaceScreen;
